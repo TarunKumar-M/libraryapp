6b658e31517c7b232c0e096cdebbe9b57d3f33e8
tarun <mallangitarun@gmail.com>
2025-05-07 23:05:02 +0530

    updated allowed hosts

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(pl_b(3y$@$n@(ftax4*8gzqn#w)an+98u0tn-=xv70t024td!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".render.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [".onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
